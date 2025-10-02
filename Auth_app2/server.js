b1a923775a2a9c7c09919f06fdb77b06e0a3af85
Aditya Sharma <166045834+aditya9259sh@users.noreply.github.com>
2025-10-02 16:56:31 +0530

    Update server.js

--- Auth_app2/server.js
+++ Auth_app2/server.js
@@ -138,7 +138,8 @@ app.get("/logout", (req, res) => {
 });
 
 // Start server
-const HOST = '127.0.0.1';
-app.listen(PORT, HOST, () => {
-  console.log(`🚀 Server running at http://${HOST}:${PORT}`);
+const PORT = process.env.PORT || 5000;  // Vercel assign karega env PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
